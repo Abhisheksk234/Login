173ccbc4b14b7f560d56797e492db2771d5e4d71
Abhisheksk234 <abhishekabhi1561@gmail.com>
2023-10-01 13:22:40 +0530

    first class

---
+++ src/Even.java
@@ -0,0 +1,17 @@
+public class Even {
+
+    public static void main(int num) {
+        if (num % 2 == 0) {
+            System.out.println("it is Even");
+        } else {
+            System.out.println("it is odd");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        main(10);
+    }
+}
+
